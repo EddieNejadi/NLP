8ac3b6c28d5bb0ea81324caf6960f658177d32ea
EddieNejadi <mehdi61b@gmail.com>
2014-03-01 12:51:30 +0100

    Added part2

--- 2014-03-01-lab2-Abdinejadi.py
+++ 2014-03-01-lab2-Abdinejadi.py
@@ -28,32 +28,51 @@ def print_brown_statistics(genre):
 	print "Brown genre\t\tTags\tSents\tWords\tSentlen\tWordlen                 "
 	print "--------------------------------------------------------------------------------"
 	for gen in genre:
-		# print type(brown_tagged_sents(gen))
 		tagged =  brown_tagged_sents(gen)
-		# print tagged
-		# print len(tagged)
 		pos_tags = len(set([tag for sen in tagged for (_w,tag) in sen]))
 		sentence = len(tagged)
 		words = len([w for sen in tagged for (w,_tag) in sen])
 		avg_sent = words /sentence
 		avg_word = sum([len(w) for sen in tagged for (w,_tag) in sen]) / words
-		# print "\t\t\t\t"+str(pos_tags)+ "\t" + str(sentence) + "\t" + str(words) + "\t"+ str(avg_sent) +"\t"+str(avg_word)
 		print gen + (" " * (16-len(gen))) +str(pos_tags)+ "\t\t" + str(sentence) + "\t" + str(words) + "\t"+ "{0:.2f}".format(round(avg_sent,2)) +"\t"+"{0:.2f}".format(round(avg_word,2))
 	print ""
+
+
+def print_common_tag_ngrams(genre, n, rows, simplify_tags=True):
+	print "== Part 2: N-gram statistics"
+	print ""
+	print "\t"+str(n)+"-gram\t\t\t\tFrequency\tAccum.freq."
+	frqdis = nltk.FreqDist()
+	tagged_sents = brown_tagged_sents(genre) 
+	for tag_sent in tagged_sents:
+		tags = [tag for (_w,tag) in tag_sent]
+		ngrams = nltk.ngrams(tags, n, True, True, "$")
+		frqdis.update(ngrams)
+	samples = frqdis.keys()[:rows]
+	acc_frq = 0.0
+	for s in samples:
+		acc_frq += frqdis.freq(s)
+		if n == 1:
+			print "\t" + s[0] + (" " * (20-(len(s[0])))) + "{0:.2f}%".format(round(frqdis.freq(s)*100,2)) +"\t\t"+"{0:.2f}%".format(round(acc_frq*100,2))
+		elif n == 2:
+			print "\t" + s[0] + " " + s[1] + (" " * (19-((len(s[0])+len(s[1]))))) + "{0:.2f}%".format(round(frqdis.freq(s)*100,2)) +"\t\t"+"{0:.2f}%".format(round(acc_frq*100,2))
+		elif n == 3: 
+			print "\t" + s[0] + " " + s[1] + " " + s[2] + (" " * (18-((len(s[0])+len(s[1])+len(s[2]))))) + "{0:.2f}%".format(round(frqdis.freq(s)*100,2)) +"\t\t"+"{0:.2f}%".format(round(acc_frq*100,2))
+		else:
+			print "Invalid ngrams! Please select n = 1,2,3"
+	
+
 
 def brown_tagged_sents(genre, simplify_tags=True):
     """Returns the tagged sentences of the given Brown category."""
     return nltk.corpus.brown.tagged_sents(categories=genre, simplify_tags=simplify_tags)
-
-def print_common_tag_ngrams(genre, n, rows, simplify_tags=True):
-	print "== Part 2: N-gram statistics"
-
 
 '''
 Global
 '''
 if __name__ == "__main__":
 	print "test main function " + __file__
-	print_brown_statistics(["fiction", "government", "news", "reviews"])
+	# print_brown_statistics(["fiction", "government", "news", "reviews"])
+	print_common_tag_ngrams("news", 2, 10)
 	# print_brown_statistics(["fiction"])
 	# print type(brown_tagged_sents("fiction", True))
